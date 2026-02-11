6902a0ab7d3ca2415b62330f88c7bad8b5cb0d7c
lolusoleye <iresoleye@gmail.com>
2026-02-11 10:38:26 +0000

    Add files via upload

---
+++ creategif.py
@@ -0,0 +1,8 @@
+import imageio.v3 as iio
+
+filenames = ['nyan-cat1.png','nyan-cat2.png','nyan-cat3.png']
+images = [ ]
+for filename in filenames:
+  images.append(iio.imread(filename))
+
+iio.imwrite('nyan.gif', images, duration = 10, loop = 0)
